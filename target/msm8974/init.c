922e0df796c2ead611204001713bf1d242ad2683
Minecrell <minecrell@minecrell.net>
2022-01-14 21:04:19 +0100

    target: Keep splash screen always on with lk2nd/lk1st

--- target/msm8974/init.c
+++ target/msm8974/init.c
@@ -667,6 +667,9 @@ int target_cont_splash_screen()
 {
 	uint8_t splash_screen = 0;
 	if(!splash_override) {
+#if WITH_LK2ND
+		splash_screen = 1;
+#else
 		switch(board_hardware_id())
 		{
 			case HW_PLATFORM_SURF:
@@ -681,6 +684,7 @@ int target_cont_splash_screen()
 				dprintf(SPEW, "Target_cont_splash=0\n");
 				splash_screen = 0;
 		}
+#endif
 	}
 	return splash_screen;
 }
